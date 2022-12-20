b894bf42473dbf1fce3f00351078ab5712178858
S.G. Donders <you@example.com>
2022-12-20 11:49:27 +0100

    first commit

---
+++ src/main/java/com/fsdeindopdracht/FsdEindopdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.fsdeindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdEindopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdEindopdrachtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsdeindopdracht/FsdEindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdeindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdEindopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
